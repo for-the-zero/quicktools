154501c27522bcdaca870b15da7f172c6c2e5f03
for-the-zero <ourmicroid@outlook.com>
2024-03-24 16:43:58 +0800

    cnm要上学了，就做这点先吧

--- calendar/index.js
+++ calendar/index.js
@@ -1,2 +1,23 @@
-setColorScheme('#FFAA2F');
 const $ = mdui.$;
+mdui.setColorScheme('#FFAA2F');
+
+const ele_nav_b = $('mdui-navigation-bar');
+const ele_nav_r = $('mdui-navigation-rail');
+
+ele_nav_b.on('change',function(){
+    changepage(ele_nav_b.attr('value'));
+    setTimeout(function(){
+        ele_nav_r.attr('value',ele_nav_b.attr('value'));
+    },200);
+});
+ele_nav_r.on('change',function(){
+    changepage(ele_nav_r.attr('value'));
+    setTimeout(function(){
+        ele_nav_b.attr('value',ele_nav_r.attr('value'));
+    },200);
+});
+
+
+function changepage(page){
+    //TODO: changepage
+}
