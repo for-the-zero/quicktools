0823136abb218da419fff12ba696b4106ba14a02
for-the-zero <forthezero@outlook.com>
2026-04-18 17:35:00 +0800

    样式小练手

--- proj/efhp/index.js
+++ proj/efhp/index.js
@@ -1,9 +1,10 @@
+import * as monaco from 'https://cdn.jsdelivr.net/npm/monaco-editor@0.45.0/+esm';
+import defaults from "./defaults.js";
 
-// import * as monaco from 'https://cdn.jsdelivr.net/npm/monaco-editor@0.45.0/+esm';
-// const monacoEditor = monaco.editor.create(document.getElementById('editor1'), {
-//     value: '# 你好\n\naaa',
-//     language: 'markdown',
-//     theme: 'vs-light',
-//     automaticLayout: false,
-//     minimap: { enabled: false }
-// });
+const monacoEditor = monaco.editor.create(document.getElementById('editor'), {
+    value: defaults.template,
+    language: 'markdown',
+    theme: 'vs-light',
+    automaticLayout: false,
+    minimap: { enabled: false }
+});
